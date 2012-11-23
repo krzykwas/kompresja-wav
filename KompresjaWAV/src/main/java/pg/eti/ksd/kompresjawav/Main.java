f24af8bc4bc728b29e2f9d39a88e53b108a8dad1
Krzysztof Kwaśniewski <krzykwas@gmail.com>
2012-11-24 00:03:08 +0100

    Pusty projekt.

---
+++ KompresjaWAV/src/main/java/pg/eti/ksd/kompresjawav/Main.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pg.eti.ksd.kompresjawav;
+
+/**
+ *
+ * @author krzykwas
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
